979ee9bcda6ae724e4d5c229bd9ac2edab53154a
HuyHoangI4t <152394866+HuyHoangI4t@users.noreply.github.com>
2024-11-30 01:46:57 +0700

    fuckkkk

--- ChildView.cpp
+++ ChildView.cpp
@@ -1,8 +1,4 @@
-﻿
-// ChildView.cpp : implementation of the CChildView class
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include "framework.h"
 #include "Bomb.h"
 #include "ChildView.h"
@@ -11,57 +7,40 @@
 #define new DEBUG_NEW
 #endif
 
-
-// CChildView
-
-CChildView::CChildView() : size(50)
-{
-}
-
-CChildView::~CChildView()
-{
-}
-
-
 BEGIN_MESSAGE_MAP(CChildView, CWnd)
-	ON_WM_PAINT()
-	ON_WM_KEYDOWN()
-	ON_WM_TIMER()
+    ON_WM_PAINT()
+    ON_WM_KEYDOWN()
+    ON_WM_TIMER()
 END_MESSAGE_MAP()
 
+CChildView::CChildView() : size(50), startTime(0) {}
 
+CChildView::~CChildView() {}
 
-// CChildView message handlers
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) {
+    if (!CWnd::PreCreateWindow(cs))
+        return FALSE;
 
-BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
-{
-	if (!CWnd::PreCreateWindow(cs))
-		return FALSE;
+    cs.dwExStyle |= WS_EX_CLIENTEDGE;
+    cs.style &= ~WS_BORDER;
+    cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS,
+        ::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1), nullptr);
 
-	cs.dwExStyle |= WS_EX_CLIENTEDGE;
-	cs.style &= ~WS_BORDER;
-	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
-		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
-
-	return TRUE;
+    return TRUE;
 }
 
-void CChildView::OnPaint() 
-{
-	CPaintDC dc(this); // device context for painting
-	
-	maze.draw(&dc);
-	player.Draw(&dc);
+void CChildView::OnPaint() {
+    CPaintDC dc(this); // device context for painting
 
+    maze.draw(&dc);
+    player.Draw(&dc);
+    Bom.Draw(&dc); // Vẽ bom
+    
     // Vẽ chuỗi thời gian tại vị trí (10, 10)
-
 }
 
-
-void CChildView::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
-{
-    CRect oldCell(player.getX() * size, player.getY() * size,
-        (player.getX() + 1) * size, (player.getY() + 1) * size);
+void CChildView::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags) {
+    CRect oldCell(player.getX() * size, player.getY() * size, (player.getX() + 1) * size, (player.getY() + 1) * size);
 
     switch (nChar)
     {
@@ -81,22 +60,31 @@ void CChildView::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
         if (player.getX() < maze.GetCols() - 1 && maze.GetCell(player.getY(), player.getX() + 1) != 1 && maze.GetCell(player.getY(), player.getX() + 1) != 2)
             player.setX(player.getX() + 1);
         break;
+
     case VK_SPACE:
-        
+        if (!Bom.GetActiveState()) {
+            Bom.SetPosition(player.getX(), player.getY());
+            Bom.SetRange(3); // Phạm vi bom có thể điều chỉnh
+            Bom.Activate(); // Kích hoạt bom
+            SetTimer(1, 2000, NULL); // 2 giây để bom nổ
+        }
         break;
     }
-
+    
     CRect newCell(player.getX() * size, player.getY() * size,
         (player.getX() + 1) * size, (player.getY() + 1) * size);
-
-    // Chỉ vẽ lại vùng cũ và vùng mới
-    InvalidateRect(oldCell, FALSE);
-    InvalidateRect(newCell, FALSE);
-  
+    InvalidateRect(oldCell | newCell,  1 );
+    
 }
 
-void CChildView::OnTimer(UINT_PTR nIDEvent)
-{
+void CChildView::OnTimer(UINT_PTR nIDEvent) {
+    if (nIDEvent == 1) { // Bom đã đến thời gian nổ
+        Bom.Update();
+        if (Bom.IsExploded()) {
+            maze.ExplosionAndItem(Bom.GetX(), Bom.GetY(), Bom.GetRange());
+            KillTimer(1); // Dừng bộ đếm thời gian của bom sau khi nó nổ
+        }
+    }
 
-	CWnd::OnTimer(nIDEvent);
+    CWnd::OnTimer(nIDEvent);
 }
